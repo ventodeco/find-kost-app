5e713078eac562ceb490705f5a7bb46650414ce5
Yosep Novento Nugroho <noventon@mamiteam.com>
2022-04-21 09:57:35 +0700

    add main facilty

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:find_kost/pages/home_page.dart';
 import 'package:find_kost/theme.dart';
+import 'package:find_kost/widgets/facilty_item.dart';
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatelessWidget {
@@ -40,6 +41,152 @@ class DetailPage extends StatelessWidget {
                   ),
                 ],
               ),
+            ),
+            ListView(
+              children: [
+                SizedBox(
+                  height: 328,
+                ),
+                Container(
+                  // selebar layar
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(20),
+                    ),
+                    color: whiteColor,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 30,
+                      ),
+                      // NOTE : TITLE
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: edge,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Kuretakeso Hott',
+                                  style: blackTextStyle.copyWith(
+                                    fontSize: 22,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 2,
+                                ),
+                                Text.rich(
+                                  TextSpan(
+                                    text: '\$10',
+                                    style: purpleTextStyle.copyWith(
+                                      fontSize: 16,
+                                    ),
+                                    children: [
+                                      TextSpan(
+                                        text: ' / month',
+                                        style: greyTextStyle.copyWith(
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'icon_star.png',
+                                  width: 20,
+                                  color: greyColor,
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      // NOTE : MAIN FACILIY
+                      Padding(
+                        padding: EdgeInsets.only(
+                          left: edge,
+                        ),
+                        child: Text(
+                          'Main Facilities',
+                          style: blackTextStyle.copyWith(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: edge,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            FacilityItem(
+                              name: 'kitchen',
+                              imageUrl: 'icon_kitchen.png',
+                              total: 2,
+                            ),
+                            FacilityItem(
+                              name: 'bedroom',
+                              imageUrl: 'icon_bedroom.png',
+                              total: 3,
+                            ),
+                            FacilityItem(
+                              name: 'Big Lemari',
+                              imageUrl: 'icon_cupboard.png',
+                              total: 3,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             )
           ],
         ),
